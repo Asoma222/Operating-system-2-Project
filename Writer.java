33bb807dd8b03db92797a74200f20118d9c230f5
zima563 <120593131+zima563@users.noreply.github.com>
2022-12-18 16:10:50 +0200

    Add files via upload

--- Writer.java
+++ Writer.java
@@ -2,30 +2,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package unrealproblem;
+package os;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author osama
  */
-
- public class Writer implements Runnable {
-     // the threads are placed into a FIFO queue when blocked, so any starvation problems are solved.
-     public static Semaphore writeLock = new Semaphore(1 , true);
-
-    @Override
-    public void run() {
-        try {
-            writeLock.acquire();
-            System.out.println("Thread "+Thread.currentThread().getName() + " is WRITING");
-            Thread.sleep(2500);
-            System.out.println("Thread "+Thread.currentThread().getName() + " has finished WRITING");
-            writeLock.release();
-        } catch (InterruptedException e) {
-            System.out.println(e.getMessage());
+// this is the writer of the real problem 
+public class Writer implements Runnable   {
+     
+    public void run(){
+    FileOperation f = new FileOperation () ; 
+        try { 
+            f.write();
+        } catch (IOException ex) {
+           System.out.println("there's something wrong ") ; 
         }
-    }
+    
+        
+      
+       
+    
+    
 }
+}
+
