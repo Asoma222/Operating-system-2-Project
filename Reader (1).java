350c92b22ceea45f08c23b9cc0e75596bef9e41c
ElRayan1 <120592949+ElRayan1@users.noreply.github.com>
2022-12-18 15:56:43 +0200

    Add files via upload
    un real world reader

---
+++ Reader (1).java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unrealproblem;
+
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author osama
+ */
+
+
+import java.util.concurrent.Semaphore;
+
+public class Reader implements Runnable {
+    //the threads are placed into a FIFO queue when blocked, so any starvation problems are solved.
+    public  static Semaphore readLock = new Semaphore(1 , true);
+    public volatile static int readCount = 0; //every thread will read/write the value in main memory only. (atomic)
+
+    @Override
+    public void run() {
+        try {
+            //Acquire Section
+            readLock.acquire();
+            synchronized(this) { //only thread it can proceed, otherwise it will wait.
+                readCount++;
+            }
+            //tack the lock from Writer and prevent from enter with Reader, so any deadlock problems are solved.
+            if (readCount == 1) {
+                Writer.writeLock.acquire();
+            }
+            readLock.release();
+
+            //Reading section
+            System.out.println("Thread "+Thread.currentThread().getName() + " is READING");
+            Thread.sleep(1500);
+            System.out.println("Thread "+Thread.currentThread().getName() + " has FINISHED READING");
+
+            //Releasing section
+            readLock.acquire();
+            synchronized(this) {
+                readCount--;
+            }
+            //leave the lock to Writer and allow to enter after Reader finish, so any deadlock problems are solved.
+            if(readCount == 0) {
+                Writer.writeLock.release();
+            }
+            readLock.release();
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
